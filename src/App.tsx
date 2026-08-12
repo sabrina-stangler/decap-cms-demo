f5ae6d228064ed45c9f0b08ad86ed84dd47c7b72
Sabrina Stangler <sabrinastangler51@gmail.com>
2026-08-12 15:48:33 -0500

    launch w/ decap attempt #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+
+import CMS from 'decap-cms-app'
+// Initialize the CMS object
+CMS.init()
+// Now the registry is available via the CMS object.
+// CMS.registerPreviewTemplate('my-template', MyTemplate)
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/decap-cms-demo'>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
