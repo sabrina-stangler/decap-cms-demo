e4e51f96c8cbab3b40c689bec39b96512f44620f
Sabrina Stangler <sabrinastangler51@gmail.com>
2026-08-14 11:35:26 -0500

    add form to see if netlify detects it

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,32 @@
 import Events from '../components/Events'
 
 export default function Home() {
+  const handleContactSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // Handle form submission logic here
+    console.log('contact form submitted', e)
+  }
+
   return (
     <div className='w-full'>
       {/* Events Section */}
       <Events />
+
+      <form name='contact' netlify onSubmit={handleContactSubmit}>
+        <p>
+          <label>
+            Name <input type='text' name='name' />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email <input type='email' name='email' />
+          </label>
+        </p>
+        <p>
+          <button type='submit'>Send</button>
+        </p>
+      </form>
     </div>
   )
 }
